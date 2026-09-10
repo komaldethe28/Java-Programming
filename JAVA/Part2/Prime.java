a853c3fcaf2f59af77b61bdb16b1ffc213a8ab8f
komal <dethekomalv@gmail.com>
2026-09-11 00:08:14 +0530

    Refactor Prime number checking logic and improve readability

--- JAVA/Part2/Prime.java
+++ JAVA/Part2/Prime.java
@@ -1,15 +1,20 @@
-import java.util.Scanner; 
-class Prime{
-public static void main(String [] args ){
-	Scanner sc = new Scanner (System.in);
-	System.out.print("Enter a number:");
-	int num= sc.nextInt();
-	Boolean op= isPrime(num);
-	System.out.print(op?num+" is Prime": num+" is not Prime");
-	}
-public static boolean isPrime(int num, int i){
-	if(num%i==0 && num!=2 || num<2) return false;
-	if (i>num/2) return true;
-	return isPrime(num, ++i);
-	}
+import java.util.Scanner;
+class Prime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        boolean op = isPrime(num, 2);
+        System.out.print(op ? num + " is Prime" : num + " is not Prime");
+    }
+
+    public static boolean isPrime(int num, int i) {
+        if (num < 2)
+            return false;
+        if (i > num / 2)
+            return true;
+        if (num % i == 0)
+            return false;
+        return isPrime(num, i + 1);
+    }
 }
